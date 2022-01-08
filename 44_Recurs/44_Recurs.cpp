1c3f7c4fa93b99af4c18749c2eb3a83166713bbd
Gevork Sofoyan <aab8117@gmail.com>
2022-01-08 13:34:47 +0300

    Добавьте файлы проекта.

---
+++ 44_Recurs/44_Recurs.cpp
@@ -0,0 +1,24 @@
+﻿// 44_Recurs.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//https://www.youtube.com/watch?v=44y44mniCJ4&list=PLQOaTSbfxUtCrKs0nicOg2npJQYSPGO9r&index=54
+//Рекурсия. Факториал числа c++ рекурсивно. Рекурсия факториал. Рекурсивный алгоритм факториал.
+
+#include <iostream>
+using namespace std;
+
+/*
+N! = N * (N-1)!
+*/
+
+int fact(int N)
+{
+    if (N == 0)
+        return 0;
+    if (N == 1)
+        return 1;
+    return N * fact(N - 1);
+}
+int main()
+{
+    cout << fact(5)<<endl;
+}
+
